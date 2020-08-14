de5a7a032cd8a9bbd2efcf23dac405d7de54dc26
Brent.Mikkelsen <Brent.Mikkelsen@lairdconnect.com>
2020-08-14 16:06:14 -0500

    update the ble file to put together review

--- apps/peripheral_IO/src/BleTask.c
+++ apps/peripheral_IO/src/BleTask.c
@@ -26,6 +26,7 @@
 
 #include "UserCommTask.h"
 #include "BleTask.h"
+#include "LairdAdvFormat.h"
 
 #include <logging/log.h>
 #define LOG_LEVEL LOG_LEVEL_DBG
@@ -49,7 +50,19 @@ LOG_MODULE_REGISTER(BleTask);
   #define BLE_TASK_QUEUE_DEPTH 8
 #endif
 
+#define BLE_ADV_LENGTH_MANUFACTURER_SPECIFIC            24
+
 #define MAX_SENSOR_NAME_LENGTH      (23+1)
+
+
+#define UINT16_BYTE_0(v) (uint8_t) (((v) & 0x00FF) >> 0)
+#define UINT16_BYTE_1(v) (uint8_t) (((v) & 0xFF00) >> 8)
+
+#define UINT32_BYTE_0(v) (uint8_t) (((v) & 0x000000FF) >> 0)
+#define UINT32_BYTE_1(v) (uint8_t) (((v) & 0x0000FF00) >> 8)
+#define UINT32_BYTE_2(v) (uint8_t) (((v) & 0x00FF0000) >> 16)
+#define UINT32_BYTE_3(v) (uint8_t) (((v) & 0xFF000000) >> 24)
+
 
 typedef struct BleTaskTag
 {
@@ -90,18 +103,50 @@ K_MSGQ_DEFINE(bleTaskQueue,
               BLE_TASK_QUEUE_DEPTH, 
               FWK_QUEUE_ALIGNMENT);
 
+//Advertisment Protocol Parameters
+struct standAdvData
+{
+
+   uint8_t companyId[2];
+   uint8_t protocolId[2];
+   uint8_t networkId[2];
+   uint8_t flags[2];
+   uint8_t bdAddress[6];
+   uint8_t recordType;
+   uint8_t recordNumber[2];
+   uint8_t epoch[4];
+   uint8_t data[4];
+   uint8_t resetCount;
+
+};
+struct standAdvData mfgData[] =
+{
+  {
+    .companyId =  {0xaa, 0xbb },
+    .protocolId = {0xcc, 0xdd },
+    .networkId =  {0xee, 0xff },
+    .flags =      {0x00, 0x11},
+    .bdAddress =  {0x22, 0x33, 0x44, 0x55, 0x66, 0x77},
+    .recordType = 0x88,
+    .epoch =      {0x99, 0xAA, 0xBB, 0xCC},
+    .data =       {0xDD, 0xEE, 0xFF, 0xFF},
+    .resetCount = 0x00,
+  }
+};
+
 static struct bt_data standardAdvert[] = 
 {
   BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-  //BT_DATA_BYTES(BT_DATA_FLAGS
+  BT_DATA(BT_DATA_MANUFACTURER_DATA, mfgData, BLE_ADV_LENGTH_MANUFACTURER_SPECIFIC)
+
 };
 
 static struct bt_le_scan_param scan_param = 
 {
-		.type       = BT_HCI_LE_SCAN_PASSIVE,
-		.options    = BT_LE_SCAN_OPT_NONE,
-		.interval   = 0x0010,
-		.window     = 0x0010,
+  .type       = BT_HCI_LE_SCAN_PASSIVE,
+  .options    = BT_LE_SCAN_OPT_NONE,
+  .interval   = 0x0010,
+  .window     = 0x0010,
 };
 /******************************************************************************/
 /* Local Function Prototypes                                                  */
@@ -109,6 +154,7 @@ static struct bt_le_scan_param scan_param =
 static void BleTaskThread(void *, void *, void *);
 static void ConfigureAndStartBle(void);
 static void ParamsInit(void);
+static void AdvertisingInit(void);
 static void AdvertisementEncoder(void);
 static void BleReadyCB(int err);
 static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
@@ -188,14 +234,6 @@ static void ConfigureAndStartBle(void)
 {  
     uint32_t err_code;
   
-  // Configure the BLE stack using the default settings.
-  // Fetch the start address of the application RAM.
-  //uint32_t ramStart = 0;
-  //err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ramStart);
-  //FRAMEWORK_ASSERT(err_code == NRF_SUCCESS);
-
-  //ConfigureMaxPacketsPerConnectionEvent(ramStart);
-
   // This must occur before the initialization items below.
   err_code = bt_enable(BleReadyCB);
   if (err_code) 
@@ -205,6 +243,7 @@ static void ConfigureAndStartBle(void)
       //FRAMEWORK_ASSERT(err_code == NRF_SUCCESS); 
       return;
   }
+  AdvertisingInit();
 }
 static void ParamsInit(void)
 {
@@ -227,14 +266,63 @@ static void ParamsInit(void)
   }
 
 }
-#define OFFSETFLAG 4
+static void AdvertisingInit(void)
+{
+  mfgData->companyId[0] = UINT16_BYTE_0(LAIRD_COMPANY_ID1);
+  mfgData->companyId[1] = UINT16_BYTE_1(LAIRD_COMPANY_ID1);
+
+}
 static void AdvertisementEncoder(void)
 {
   size_t index = 0; 
-  //struct bt_data adTest[1];
 
- // index++;// = index + OFFSETFLAG;
-  //adTest[0] = {BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0x00, 0x03)};
+  // protocol id
+  mfgData->protocolId[0] = UINT16_BYTE_0(LAIRD_ADV_PROTOCOL_ID_SENTRIUS);
+  mfgData->protocolId[1] = UINT16_BYTE_1(LAIRD_ADV_PROTOCOL_ID_SENTRIUS);
+
+  // network id
+  uint32_t networkId = 0;
+  //AttributeTask_GetUint32(&networkId, ATTR_INDEX_networkId);
+  mfgData->networkId[0] = UINT32_BYTE_0(networkId);
+  mfgData->networkId[1] = UINT32_BYTE_1(networkId);
+
+  // flags
+  uint32_t flags = Flags_Get();
+  mfgData->flags[0] = UINT32_BYTE_0(flags);
+  mfgData->flags[1] = UINT32_BYTE_1(flags);
+
+  // BD addr
+  mfgData->bdAddress[0] = bleObj.bdAddr.addr[0];
+  mfgData->bdAddress[1] = bleObj.bdAddr.addr[1];
+  mfgData->bdAddress[2] = bleObj.bdAddr.addr[2];
+  mfgData->bdAddress[3] = bleObj.bdAddr.addr[3];
+  mfgData->bdAddress[4] = bleObj.bdAddr.addr[4];
+  mfgData->bdAddress[5] = bleObj.bdAddr.addr[5];
+
+  // event type
+  mfgData->recordType =pSensorMsg->event.type;
+
+  // id
+  mfgData->recordNumber[0] = UINT16_BYTE_0(pSensorMsg->id);
+  mfgData->recordNumber[1] = UINT16_BYTE_1(pSensorMsg->id);
+
+  // epoch
+  mfgData->epoch[0] = UINT32_BYTE_0(pSensorMsg->event.timestamp);
+  mfgData->epoch[1] = UINT32_BYTE_1(pSensorMsg->event.timestamp);
+  mfgData->epoch[2] = UINT32_BYTE_2(pSensorMsg->event.timestamp);
+  mfgData->epoch[3] = UINT32_BYTE_3(pSensorMsg->event.timestamp);
+
+  // data
+  mfgData->data[0] = UINT16_BYTE_0(pSensorMsg->event.data.u16);
+  mfgData->data[1] = UINT16_BYTE_1(pSensorMsg->event.data.u16);
+  mfgData->data[2] = 0;
+  mfgData->data[3] = 0;
+
+  // reset count LSB
+  uint32_t resetCount = 0;
+  //AttributeTask_GetUint32(&resetCount, ATTR_INDEX_resetCount);
+  mfgData->resetCount = UINT32_BYTE_0(resetCount);
+
 }
 static void BleReadyCB(int err)
 {
@@ -242,6 +330,7 @@ static void BleReadyCB(int err)
   if (IS_ENABLED(CONFIG_SETTINGS)) 
   {
     ParamsInit();
+    
     settings_load();
     FRAMEWORK_MSG_UNICAST_CREATE_AND_SEND(FWK_ID_BLE_TASK, FMC_CODE_SENSOR);
   }
@@ -278,7 +367,7 @@ static DispatchResult_t BleReadyMsgHandler(FwkMsgReceiver_t *pMsgRxer, FwkMsg_t 
         {
             printk("Advertising failed to start (err %d)\n", err);
             LOG_ERR("Advertising failed to start (err %d)\n", err);
-            return;
+            return 0;
         }
         LOG_DBG("Advertising\n");
     }
