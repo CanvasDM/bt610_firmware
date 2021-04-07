2e561900d545639af163a799725d28b1a9ce5e26
Greg Leach <greg.leach@lairdconnect.com>
2021-04-07 07:55:54 +0100

    Missed earlier commits.

--- common/include/AlarmControl.h
+++ common/include/AlarmControl.h
@@ -29,19 +29,57 @@ extern "C" {
 /* Global Function Prototypes                                                 */
 /******************************************************************************/
 /**
- * @brief
+ * @brief Check the Thermistor temperture against the two high thresholds
  *
- * @param
- * @param
+ * @param channel This is the thermistor channel
  *
- * @retval
+ * @retval negative error code, 0 on success
  */
 int HighTempAlarmCheck(size_t channel);
+/**
+ * @brief Check the Thermistor temperture against the two low thresholds
+ *
+ * @param channel This is the thermistor channel
+ *
+ * @retval negative error code, 0 on success
+ */
 int LowTempAlarmCheck(size_t channel);
+/**
+ * @brief Check the delta Temperture measurment against the threshold
+ *
+ * @param channel This is the thermistor channel
+ *
+ * @param channel This is the delta temperture
+ *
+ * @retval negative error code, 0 on success
+ */
 int DeltaTempAlarmCheck(size_t channel, float tempDifference);
+/**
+ * @brief Check the Analog value against the two high thresholds
+ *
+ * @param channel This is the analog channel
+ *
+ * @retval negative error code, 0 on success
+ */
 int HighAnalogAlarmCheck(size_t channel);
+/**
+ * @brief Check the Analog value against the two low thresholds
+ *
+ * @param channel This is the analog channel
+ *
+ * @retval negative error code, 0 on success
+ */
 int LowAnalogAlarmCheck(size_t channel);
-int DeltaAnalogAlarmCheck(size_t channel, float tempDifference);
+/**
+ * @brief Check the delta analog measurment value against the threshold
+ *
+ * @param channel This is the analog channel
+ *
+ * @param channel This is the delta analog value
+ *
+ * @retval negative error code, 0 on success
+ */
+int DeltaAnalogAlarmCheck(size_t channel, float analogDifference);
 
 #ifdef __cplusplus
 }
---
+++ common/include/Flags.h
@@ -0,0 +1,72 @@
+/**
+ * @file Flags.h
+ * @brief
+ *
+ * Copyright (c) 2020 Laird Connectivity
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __FLAGS_Hv__
+#define __FLAGS_H__
+
+/******************************************************************************/
+/* Includes                                                                   */
+/******************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+/* Global Constants, Macros and Type Definitions                              */
+/******************************************************************************/
+#define FLAG_TIME_WAS_SET              0x1, 0
+#define FLAG_ACTIVE_MODE               0x1, 1
+#define FLAG_ANY_ALARM                 0x1, 2
+#define FLAG_LOW_BATTERY_ALARM         0x1, 3
+#define FLAG_TEMP_ALARM_START_BIT      4
+#define FLAG_TEMP1_ALARM               0x1, 4
+#define FLAG_TEMP2_ALARM               0x1, 5
+#define FLAG_TEMP3_ALARM               0x1, 6
+#define FLAG_TEMP4_ALARM               0x1, 7
+#define FLAG_ANALOG_ALARM_START_BIT    8
+#define FLAG_ANALOG1_ALARM             0x1, 8
+#define FLAG_ANALOG2_ALARM             0x1, 9
+#define FLAG_ANALOG3_ALARM             0x1, 10
+#define FLAG_ANALOG4_ALARM             0x1, 11
+#define FLAG_DIGITAL_IN1_STATE         0x1, 12
+#define FLAG_DIGITAL_IN2_STATE         0x1, 13
+#define FLAG_MAGNET_STATE              0x1, 14
+#define FLAG_TAMPER_SWITCH_STATE       0x1, 15
+
+#define ANY_ALARM_MASK    0x00007F10
+#define TEMP_ALARM_MASK   0x1
+#define ANALOG_ALARM_MASK 0x1
+/******************************************************************************/
+/* Global Data Definitions                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* Global Function Prototypes                                                 */
+/******************************************************************************/
+//-----------------------------------------------
+//! @brief  This module uses a mutex.
+//!
+void Flags_Init(void);
+
+//-----------------------------------------------
+//! @brief  Set a single or multi-bit flag.  Handles setting of any alarm flag based upon
+//! ANY_ALARM_MASK.
+//!
+void Flags_Set(uint32_t Mask, uint32_t Position, uint32_t Value);
+
+//-----------------------------------------------
+//! @retval The current value of the flags.
+//!
+uint32_t Flags_Get(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FLAGS_H__ */
