a93b9037c392677772389f38c05f15d16d5b5c05
Brent.Mikkelsen <Brent.Mikkelsen@lairdconnect.com>
2021-04-13 08:19:32 -0500

    added coded phy off as default

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -18,7 +18,7 @@ extern "C" {
 /******************************************************************************/
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 5
-#define VERSION_PATCH 37
+#define VERSION_PATCH 38
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
