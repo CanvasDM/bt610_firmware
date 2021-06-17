4cd87e3f0d67d08b61997147d0915ac59d538b2a
Brent.Mikkelsen <Brent.Mikkelsen@lairdconnect.com>
2021-06-17 17:02:55 -0500

    same issue as before just removed test code
    (cherry picked from commit 836b045c0c105cd104a250661dbf028e823542cb)

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -18,7 +18,7 @@ extern "C" {
 /******************************************************************************/
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 17
-#define VERSION_PATCH 49
+#define VERSION_PATCH 51
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
