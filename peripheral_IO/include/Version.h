2fdaff61f594ca0f347d2ddbcaead0488843a4e1
Brent.Mikkelsen <Brent.Mikkelsen@lairdconnect.com>
2021-07-08 09:22:57 -0500

    trying to fix the issues with event not showing anything other than TEMP1 in the event advert
    (cherry picked from commit 95894afcd6e1ab4c54ea92e11bf807d65bbe8817)

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -17,8 +17,8 @@ extern "C" {
 /* Application Firmware Version                                               */
 /******************************************************************************/
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 18
-#define VERSION_PATCH 87
+#define VERSION_MINOR 19
+#define VERSION_PATCH 28
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
