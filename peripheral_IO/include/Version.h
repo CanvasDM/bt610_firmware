55b02f64d4f6c8ae14887f03a451b4c7c99077ae
Brent.Mikkelsen <Brent.Mikkelsen@lairdconnect.com>
2021-07-16 10:47:44 -0500

    Bug 19414 - Analog current not configured correctly By calling temp timer twice at init and when set to I high rate caused issues with other timers

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -17,8 +17,8 @@ extern "C" {
 /* Application Firmware Version                                               */
 /******************************************************************************/
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 19
-#define VERSION_PATCH 69
+#define VERSION_MINOR 20
+#define VERSION_PATCH 7
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
