3500509a109da648f445a3b1b451ad5a67611a57
Jamie McCrae <jamie.mccrae@lairdconnect.com>
2021-09-16 13:22:08 +0100

    Bug #20777 Version 1.26.4 release

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -18,7 +18,7 @@ extern "C" {
 /******************************************************************************/
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 26
-#define VERSION_PATCH 3
+#define VERSION_PATCH 4
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
