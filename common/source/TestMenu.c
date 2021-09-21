c6118777d7236b224db2f4f03afdf83814aadc05
Greg Leach <greg.leach@lairdconnect.com>
2021-09-21 17:51:31 +0100

    Bug #19931 Add test menu input validation

--- common/source/TestMenu.c
+++ common/source/TestMenu.c
@@ -32,6 +32,20 @@ LOG_MODULE_REGISTER(TestMenu);
 #include "FrameworkIncludes.h"
 
 /******************************************************************************/
+// Local Constant, Macro and Type Definitions
+/******************************************************************************/
+#define TEST_MENU_VIN_ARG_COUNT 2
+#define TEST_MENU_CIN_ARG_COUNT 2
+#define TEST_MENU_THERM_ARG_COUNT 2
+#define TEST_MENU_TEMP_ARG_COUNT 2
+#define TEST_MENU_CONFIGURE_ARG_COUNT 3
+#define TEST_MENU_FIVE_SET_ARG_COUNT 2
+#define TEST_MENU_BATTERY_SET_ARG_COUNT 2
+#define TEST_MENU_DIGITAL_ENABLE_ARG_COUNT 2
+#define TEST_MENU_CAL_ARG_COUNT 3
+#define TEST_MENU_I2C_PULL_ARG_COUNT 2
+
+/******************************************************************************/
 /* Local Data Definitions                                                     */
 /******************************************************************************/
 static int samples = 1;
@@ -134,41 +148,70 @@ static int convert_channel(const char *str)
 
 static int vin(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int ch = convert_channel(argv[1]);
-	AdcMeasurementType_t type = ADC_TYPE_VOLTAGE;
-	shell_print(shell, "ch: %d type: %s", ch, AdcBt6_GetTypeString(type));
-	return sample_with_channel(shell, ch - 1, type, AdcBt6_ConvertVoltage);
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_VIN_ARG_COUNT) {
+		shell_print(shell, "Please enter a channel from 1 - 4");
+	} else {
+		int ch = convert_channel(argv[1]);
+		AdcMeasurementType_t type = ADC_TYPE_VOLTAGE;
+		shell_print(shell, "ch: %d type: %s", ch,
+			    AdcBt6_GetTypeString(type));
+		result = sample_with_channel(shell, ch - 1, type,
+					     AdcBt6_ConvertVoltage);
+	}
+	return result;
 }
 
 static int cin(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int ch = convert_channel(argv[1]);
-	AdcMeasurementType_t type = ADC_TYPE_CURRENT;
-	shell_print(shell, "ch: %d type: %s", ch, AdcBt6_GetTypeString(type));
-	return sample_with_channel(shell, ch - 1, type, AdcBt6_ConvertCurrent);
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_CIN_ARG_COUNT) {
+		shell_print(shell, "Please enter a channel from 1 - 4");
+	} else {
+		int ch = convert_channel(argv[1]);
+		AdcMeasurementType_t type = ADC_TYPE_CURRENT;
+		shell_print(shell, "ch: %d type: %s", ch,
+			    AdcBt6_GetTypeString(type));
+		result = sample_with_channel(shell, ch - 1, type,
+					     AdcBt6_ConvertCurrent);
+	}
+	return result;
 }
 
 static int therm(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int ch = convert_channel(argv[1]);
-	AdcMeasurementType_t type = ADC_TYPE_THERMISTOR;
-	shell_print(shell, "ch: %d type: %s", ch, AdcBt6_GetTypeString(type));
-	return sample_no_channel(shell, type,
-				 AdcBt6_ApplyThermistorCalibration);
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_THERM_ARG_COUNT) {
+		shell_print(shell, "Please enter a channel from 1 - 4");
+	} else {
+		int ch = convert_channel(argv[1]);
+		AdcMeasurementType_t type = ADC_TYPE_THERMISTOR;
+		shell_print(shell, "ch: %d type: %s", ch,
+			    AdcBt6_GetTypeString(type));
+		result = sample_no_channel(shell, type,
+					   AdcBt6_ApplyThermistorCalibration);
+	}
+	return result;
 }
 
 static int temp(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int ch = convert_channel(argv[1]);
-	AdcMeasurementType_t type = ADC_TYPE_THERMISTOR;
-	shell_print(shell, "ch: %d type: %s", ch, AdcBt6_GetTypeString(type));
-	int rc = sample_with_channel(shell, ch - 1, type,
-				     AdcBt6_ConvertThermToTemperature);
-	return rc;
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_TEMP_ARG_COUNT) {
+		shell_print(shell, "Please enter a channel from 1 - 4");
+	} else {
+		int ch = convert_channel(argv[1]);
+		AdcMeasurementType_t type = ADC_TYPE_THERMISTOR;
+		shell_print(shell, "ch: %d type: %s", ch,
+			    AdcBt6_GetTypeString(type));
+		result = sample_with_channel(shell, ch - 1, type,
+					     AdcBt6_ConvertThermToTemperature);
+	}
+	return result;
 }
 
 static int vref(const struct shell *shell, size_t argc, char **argv)
@@ -181,30 +224,52 @@ static int vref(const struct shell *shell, size_t argc, char **argv)
 
 static int configure(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	samples = MAX(1, atoi(argv[1]));
-	delay = MAX(1, atoi(argv[2]));
-	shell_print(shell, "samples: %d delay (ms): %d", samples, delay);
-	return 0;
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_CONFIGURE_ARG_COUNT) {
+		shell_print(shell,
+			    "Please enter a sample count and delay time");
+	} else {
+		samples = MAX(1, atoi(argv[1]));
+		delay = MAX(1, atoi(argv[2]));
+		shell_print(shell, "samples: %d delay (ms): %d", samples,
+			    delay);
+		result = 0;
+	}
+	return result;
 }
 
 static int fiveSet(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int value = atoi(argv[1]);
-	int rc = (value == 0) ? AdcBt6_FiveVoltDisable() :
-				      AdcBt6_FiveVoltEnable();
-	shell_print(shell, "Set 5V: %d status: %d", value, rc);
-	return 0;
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_FIVE_SET_ARG_COUNT) {
+		shell_print(shell, "Please enter 0 for off or 1 for on");
+	} else {
+		int value = atoi(argv[1]);
+		int rc = (value == 0) ? AdcBt6_FiveVoltDisable() :
+					      AdcBt6_FiveVoltEnable();
+		shell_print(shell, "Set 5V: %d status: %d", value, rc);
+		result = 0;
+	}
+	return result;
 }
 
 static int batterySet(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int value = atoi(argv[1]);
-	int rc = (value == 0) ? AdcBt6_BplusDisable() : AdcBt6_BplusEnable();
-	shell_print(shell, "Set Battery Enable: %d status: %d", value, rc);
-	return 0;
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_BATTERY_SET_ARG_COUNT) {
+		shell_print(shell, "Please enter 0 for off or 1 for on");
+	} else {
+		int value = atoi(argv[1]);
+		int rc = (value == 0) ? AdcBt6_BplusDisable() :
+					      AdcBt6_BplusEnable();
+		shell_print(shell, "Set Battery Enable: %d status: %d", value,
+			    rc);
+		result = 0;
+	}
+	return result;
 }
 
 static int DigitalOutputToggle(const struct shell *shell, size_t argc,
@@ -220,37 +285,49 @@ static int DigitalOutputToggle(const struct shell *shell, size_t argc,
 
 static int digitalEnable(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	int value = atoi(argv[1]);
-	int status1 = BSP_PinSet(DIN1_ENABLE_PIN, value);
-	int status2 = BSP_PinSet(DIN2_ENABLE_PIN, value);
-	shell_print(shell, "Set To DIN_EN: %d %d %d", value, status1, status2);
-	return 0;
+	int result = -EINVAL;
+
+	if (argc < TEST_MENU_DIGITAL_ENABLE_ARG_COUNT) {
+		shell_print(shell, "Please enter 0 for off or 1 for on");
+	} else {
+		int value = atoi(argv[1]);
+		int status1 = BSP_PinSet(DIN1_ENABLE_PIN, value);
+		int status2 = BSP_PinSet(DIN2_ENABLE_PIN, value);
+		shell_print(shell, "Set To DIN_EN: %d %d %d", value, status1,
+			    status2);
+		result = 0;
+	}
+	return result;
 }
 
 static int cal(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	float c1 = atof(argv[1]);
-	float c2 = atof(argv[2]);
-	shell_print(shell, "c1 set to %.4e", c1);
-	shell_print(shell, "c2 set to %.4e", c2);
+	int result = -EINVAL;
 
-	float ge;
-	float oe;
-	int status;
-	size_t i;
-	for (i = 0; i < samples; i++) {
-		status = AdcBt6_CalibrateThermistor(c1, c2, &ge, &oe);
-		if (status != 0) {
-			shell_print(shell, "error: %d", status);
-			break;
+	if (argc < TEST_MENU_CAL_ARG_COUNT) {
+		shell_print(shell, "Please enter c1 and c2 constants values");
+	} else {
+		float c1 = atof(argv[1]);
+		float c2 = atof(argv[2]);
+		shell_print(shell, "c1 set to %.4e", c1);
+		shell_print(shell, "c2 set to %.4e", c2);
+
+		float ge;
+		float oe;
+		int status;
+		size_t i;
+		for (i = 0; i < samples; i++) {
+			status = AdcBt6_CalibrateThermistor(c1, c2, &ge, &oe);
+			if (status != 0) {
+				shell_print(shell, "error: %d", status);
+				break;
+			}
+			shell_print(shell, "ge: %.4e oe: %.4e", ge, oe);
+			k_sleep(K_MSEC(delay));
 		}
-		shell_print(shell, "ge: %.4e oe: %.4e", ge, oe);
-		k_sleep(K_MSEC(delay));
+		result = 0;
 	}
-
-	return 0;
+	return result;
 }
 
 static int advertise(const struct shell *shell, size_t argc, char **argv)
@@ -259,35 +336,34 @@ static int advertise(const struct shell *shell, size_t argc, char **argv)
 	ARG_UNUSED(argv);
 
 	shell_print(shell, "Starting advertising . . .\n");
-	FRAMEWORK_MSG_CREATE_AND_SEND(FWK_ID_USER_IF_TASK,
-						      FWK_ID_USER_IF_TASK,
-						      FMC_ENTER_ACTIVE_MODE);
+	FRAMEWORK_MSG_CREATE_AND_SEND(FWK_ID_USER_IF_TASK, FWK_ID_USER_IF_TASK,
+				      FMC_ENTER_ACTIVE_MODE);
 	return 0;
 }
 
 static int i2cpull(const struct shell *shell, size_t argc, char **argv)
 {
-	ARG_UNUSED(argc);
-	ARG_UNUSED(argv);
+	int result = -EINVAL;
 
-	shell_print(shell, "Getting an event . . .\n");
+	if (argc < TEST_MENU_I2C_PULL_ARG_COUNT) {
+		shell_print(shell, "Please enter 0 for off or 1 for on");
+	} else {
+		int state = atoi(argv[1]);
 
-	int state = atoi(argv[1]);
-
-	if (state){
-		shell_print(shell, "Enabling I2C pullups . . .\n");
-		NRF_P0->PIN_CNF[26] &= ~0xC;
-		NRF_P0->PIN_CNF[26] |= 0xC;
-		NRF_P0->PIN_CNF[27] &= ~0xC;
-		NRF_P0->PIN_CNF[27] |= 0xC;
+		if (state) {
+			shell_print(shell, "Enabling I2C pullups . . .\n");
+			NRF_P0->PIN_CNF[26] &= ~0xC;
+			NRF_P0->PIN_CNF[26] |= 0xC;
+			NRF_P0->PIN_CNF[27] &= ~0xC;
+			NRF_P0->PIN_CNF[27] |= 0xC;
+		} else {
+			shell_print(shell, "Disabling I2C pullups . . .\n");
+			NRF_P0->PIN_CNF[26] &= ~0xC;
+			NRF_P0->PIN_CNF[27] &= ~0xC;
+		}
+		result = 0;
 	}
-	else{
-
-		shell_print(shell, "Disabling I2C pullups . . .\n");
-		NRF_P0->PIN_CNF[26] &= ~0xC;
-		NRF_P0->PIN_CNF[27] &= ~0xC;
-	}
-	return 0;
+	return result;
 }
 
 /******************************************************************************/
@@ -321,8 +397,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	SHELL_CMD(configure, NULL,
 		  "Set <number of samples> <delay between samples (ms)>",
 		  configure),
-	SHELL_CMD(advertise, NULL, "Advertises . . .",advertise),
-	SHELL_CMD(i2cpull, NULL, "Configures I2C pullups . . .",i2cpull),
+	SHELL_CMD(advertise, NULL, "Advertises . . .", advertise),
+	SHELL_CMD(i2cpull, NULL, "Configures I2C pullups . . .", i2cpull),
 
 	SHELL_SUBCMD_SET_END);
 
