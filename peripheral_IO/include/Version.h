c69bac5acb5768285175bb1277209f3f0ab50b02
Jamie McCrae <jamie.mccrae@lairdconnect.com>
2021-09-22 17:37:30 +0100

    Bug #19949 Version 1.27.0 release

--- peripheral_IO/include/Version.h
+++ peripheral_IO/include/Version.h
@@ -17,8 +17,8 @@ extern "C" {
 /* Application Firmware Version                                               */
 /******************************************************************************/
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 26
-#define VERSION_PATCH 10
+#define VERSION_MINOR 27
+#define VERSION_PATCH 0
 #define VERSION_STRING                                                         \
 	STRINGIFY(VERSION_MAJOR)                                               \
 	"." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
