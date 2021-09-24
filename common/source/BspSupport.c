1bee0dd83565138bfba746857704f275e41dc5fe
Jamie McCrae <jamie.mccrae@lairdconnect.com>
2021-09-24 14:16:35 +0100

    Bug #19980 Fix BSP Input code oddity

--- common/source/BspSupport.c
+++ common/source/BspSupport.c
@@ -216,30 +216,51 @@ void BSP_ConfigureDigitalInputs(uint8_t pin, gpio_flags_t enable,
 				gpio_flags_t edge)
 {
 	int r = 0;
-	/* DIN1 */
-	if (pin == DIN1_MCU_PIN) {
-		r = gpio_pin_configure(port0, GPIO_PIN_MAP(DIN1_MCU_PIN),
-				       enable);
-		LOG_INF("DIN 1 config (%d)", r);
-		r = gpio_pin_interrupt_configure(
-			port0, GPIO_PIN_MAP(DIN1_MCU_PIN), (edge));
-		LOG_INF("DIN 1 interrupt (%d)", r);
-		gpio_init_callback(&digitalIn1_cb_data, DigitalIn1HandlerIsr,
-				   BIT(GPIO_PIN_MAP(DIN1_MCU_PIN)));
-		gpio_add_callback(port0, &digitalIn1_cb_data);
-		digital_input_1_IRQ_config = edge;
-	}
-	/* DIN2 */
-	else if (pin == DIN2_MCU_PIN) {
-		gpio_pin_configure(port1, GPIO_PIN_MAP(DIN2_MCU_PIN),
-				   GPIO_INPUT);
-		gpio_pin_interrupt_configure(port1, GPIO_PIN_MAP(DIN2_MCU_PIN),
-					     (edge));
+	const struct device *din_port = NULL;
+	void *isr_function;
+	struct gpio_callback *callback_data;
+	gpio_flags_t *pin_irq_conf;
 
-		gpio_init_callback(&digitalIn2_cb_data, DigitalIn2HandlerIsr,
-				   BIT(GPIO_PIN_MAP(DIN2_MCU_PIN)));
-		gpio_add_callback(port1, &digitalIn2_cb_data);
-		digital_input_2_IRQ_config = edge;
+	if (pin == DIN1_MCU_PIN) {
+		/* DIN1 */
+		din_port = port0;
+		isr_function = DigitalIn1HandlerIsr;
+		callback_data = &digitalIn1_cb_data;
+		pin_irq_conf = &digital_input_1_IRQ_config;
+	} else if (pin == DIN2_MCU_PIN) {
+		/* DIN2 */
+		din_port = port1;
+		isr_function = DigitalIn2HandlerIsr;
+		callback_data = &digitalIn2_cb_data;
+		pin_irq_conf = &digital_input_2_IRQ_config;
+	} else {
+		LOG_ERR("Invalid pin for digital input config: %d", pin);
+		return;
+	}
+
+	if (din_port == NULL) {
+		LOG_ERR("Port for pin %d is null", pin);
+		return;
+	}
+
+	r = gpio_pin_configure(din_port, GPIO_PIN_MAP(pin), enable);
+
+	if (r == 0) {
+		r = gpio_pin_interrupt_configure(din_port, GPIO_PIN_MAP(pin),
+						 edge);
+	}
+
+	if (r == 0) {
+		gpio_init_callback(callback_data, isr_function,
+				   BIT(GPIO_PIN_MAP(pin)));
+
+		r = gpio_add_callback(din_port, callback_data);
+
+		*pin_irq_conf = edge;
+	}
+
+	if (r != 0) {
+		LOG_ERR("Failed configuring pin %d: %d", pin, r);
 	}
 }
 
@@ -539,7 +560,7 @@ static void DigitalIn1HandlerIsr(const struct device *port,
 {
 	int pinStatus = gpio_pin_get(port0, GPIO_PIN_MAP(DIN1_MCU_PIN));
 
-	LOG_DBG("Digital pin%d is %u", DIN1_MCU_PIN, pinStatus);
+	LOG_DBG("Digital pin %d is %u", DIN1_MCU_PIN, pinStatus);
 	SendDigitalInputStatus(DIN1_MCU_PIN, pinStatus);
 }
 
@@ -549,7 +570,7 @@ static void DigitalIn2HandlerIsr(const struct device *port,
 {
 	int pinStatus = gpio_pin_get(port1, GPIO_PIN_MAP(DIN2_MCU_PIN));
 
-	LOG_DBG("Digital pin%d is %u", DIN2_MCU_PIN, pinStatus);
+	LOG_DBG("Digital pin %d is %u", DIN2_MCU_PIN, pinStatus);
 	SendDigitalInputStatus(DIN2_MCU_PIN, pinStatus);
 }
 
