922dfbb4ac60241a7ef545005a55879b65175df6
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-10-28 10:42:42 -0500

    Enable HW flow control on console UART
    bug 22968

--- src/main.c
+++ src/main.c
@@ -5,23 +5,24 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-#include <zephyr.h>
+#include <zephyr/zephyr.h>
 #include <version.h>
 #ifdef CONFIG_ATTR
-#include "attr.h"
+#include <attr.h>
 #endif
 #ifdef CONFIG_LCZ_LWM2M_CLIENT
-#include "lcz_lwm2m_client.h"
+#include <lcz_lwm2m_client.h>
 #endif
 #ifdef CONFIG_LCZ_LWM2M_FW_UPDATE
-#include "lcz_lwm2m_fw_update.h"
+#include <lcz_lwm2m_fw_update.h>
 #endif
 #include "app_version.h"
 #ifdef CONFIG_SHELL_BACKEND_SERIAL
-#include <shell/shell_uart.h>
+#include <zephyr/shell/shell_uart.h>
 #endif
+#include <zephyr/drivers/uart.h>
 #include "ControlTask.h"
 #include "BspSupport.h"
 
@@ -31,8 +32,17 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #define PKG_NAME_PREFIX "lc_dm"
 #define PKG_NAME PKG_NAME_PREFIX "-" CONFIG_BOARD "-"
 
+static const struct uart_config uart_cfg = { .baudrate = 115200,
+					     .parity = UART_CFG_PARITY_NONE,
+					     .stop_bits = UART_CFG_STOP_BITS_1,
+					     .data_bits = UART_CFG_DATA_BITS_8,
+					     .flow_ctrl = UART_CFG_FLOW_CTRL_RTS_CTS };
+
 void main(void)
 {
+	const struct device *uart_dev;
+	int ret;
+
 #ifdef CONFIG_SHELL_BACKEND_SERIAL
 	/* Disable log output by default on the UART console.
 	 * Re-enable logging using the 'log go' cmd.
@@ -45,6 +55,17 @@ void main(void)
 		log_backend_deactivate(shell_backend_uart_get_ptr()->log_backend->backend);
 	}
 #endif
+
+	uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	if (device_is_ready(uart_dev)) {
+		ret = uart_configure(uart_dev, &uart_cfg);
+		if (ret != 0) {
+			LOG_ERR("Could not config console UART [%d]", ret);
+		}
+	} else {
+		LOG_ERR("Console UART not ready");
+	}
+
 #ifdef CONFIG_LCZ_LWM2M_CLIENT
 	(void)lcz_lwm2m_client_set_device_firmware_version(APP_VERSION_STRING);
 	(void)lcz_lwm2m_client_set_software_version(KERNEL_VERSION_STRING);
